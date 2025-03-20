6ad84a776ba729690b646a319021d73b7a41106f
Balou <baloujordy28@gmail.com>
2025-03-20 11:43:51 +0100

    Premier commit de mon portfolio

--- Proets ILEC/Gbibliotheque/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.gbibliotheque {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.gbibliotheque to javafx.fxml;
-    exports org.example.gbibliotheque;
-}
